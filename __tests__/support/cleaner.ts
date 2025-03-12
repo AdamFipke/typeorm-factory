efb0a424be4daf9f6d75b9d511c21f60f64afa05
Adam Fipke <adamfipke@gmail.com>
2025-03-11 17:14:22 -0700

    deleted some stuff related to jest hopefully this fixes stuff?

--- __tests__/support/cleaner.ts
+++
@@ -1,6 +0,0 @@
-import { DataSource } from 'typeorm';
-
-export const clean = (datasource: DataSource) => {
-  const names = ['comment', 'post', 'author'];
-  return datasource.query(names.map(name => `DELETE FROM ${name};`).join('\n'));
-};
